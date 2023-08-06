1ddce2663d7633d9179c051a1a3db91a86863716
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-08-06 04:20:31 +0200

    add: new script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+let aleatoire = Math.floor(Math.random()* 10);
+switch (aleatoire) {
+    case 0:
+    
+        let userName2= prompt("Quel est votre prenom");
+        userName2;
+        let userDate2= prompt("quel age avez vous ?");
+        userDate1;
+        let aleatoire1 = Math.floor(Math.random()* 10);
+        switch (aleatoire1) {
+            case 0:
+                alert(
+                    "bbbbbzbbzbzbb"
+                )
+        alert( userName2 + " , " + " « L'inspiration existe, mais il faut qu'elle vous trouve au travail. » — Une citation de sagesse de Pablo Picasso " + " , " + " n'est ce pas une bonne citation à connaitre à " + userDate2 +  " ans !" );
+        break;
+            case 1:
+            let userName3= prompt("Quel est votre prenom");
+            userName3;
+            let userDate3= prompt("quel age avez vous ?");
+            userDate3;
+            alert( userName3 + " , " + " « La vie est un défi à relever, un bonheur à mériter, une aventure à tenter. » — une citation positive de Mère Teresa, missionnaire en Inde " + " , " + " n'est ce pas une tres tres belle  citation à connaitre à " + userDate3 +  " ans !" );
+            break;
+            
+                    case 2 :
+                        let userName4= prompt("Quel est votre prenom");
+                        userName4;
+                        let userDate4= prompt("quel age avez vous ?");
+                        userDate4;
+                        alert( userName4 + " , " + "  « Montrez-vous, montrez-vous, montrez-vous, et au bout d'un moment, la muse se montrera également. » — Une citation motivante d'Isabel Allende " + " , " + " n'est ce pas une tres tres belle  citation à connaitre à " + userDate4 +  " ans !" );
+                        break;
+
+                            case 3:
+                                let userName5= prompt("Quel est votre prenom");
+                                userName5;
+                                let userDate5= prompt("quel age avez vous ?");
+                                userDate5;
+                                alert( userName5 + " , " + "  « Montrez-vous, montrez-vous, montrez-vous, et au bout d'un moment, la muse se montrera également. » — Une citation motivante d'Isabel Allende " + " , " + " n'est ce pas une tres tres belle  citation à connaitre à " + userDate4 +  " ans !" );
+                                break;
+
+                        case 4:
+                            console.log('hulk a sept doctorat');
+                            break;
+        default:
+            console.log('je suis en manque d inspiration');
+        break;
+    }
