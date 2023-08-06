b23b8f19afaa029c93cc0c6d8e4bc716bdaa94c0
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-08-06 04:51:26 +0200

    feat: add first conditional structure

--- script.js
+++ script.js
@@ -3,4 +3,34 @@ alert( "Bien venu dans notre programme Qui Booste votre Motivatoin journaliere a
         userName;
         let userAge= prompt("quel age avez vous ?");
         userAge;
+
+        let aleatoire = Math.floor(Math.random()* 1);
+                switch (aleatoire) {
+                        case 0:
         
+                        let sage1= (" « Vous pouvez obtenir tout ce que vous désirez dans la vie si vous aidez suffisamment de personnes à obtenir ce qu'elles veulent. » — Une citation motivante de Zig Ziglar ")
+                        sage1
+                        switch (aleatoire) {
+                        case Number (userAge)> 0 && Number (userAge) <= 25 :
+                          if (Number (userAge)> 0 && Number (userAge) <= 25){
+                            alert( userName + " , " + sage1 + " , " + "n'est ce pas une tres tres belle  citation à connaitre à " + Number (userAge) +  " ans !" );
+                          }
+                        break;
+        
+                        case Number (userAge) > 25 && Number (userAge) <= 50 :
+                          if (Number (userAge) > 25 && Number (userAge) <= 50){
+                            alert( userName + " , " + sage1 + " , " + " Genial citation à connaitre maintenant que tu as " + Number (userAge) +  " ans !" );
+                          }
+                        break;
+                          
+                        case (Number (userAge) > 50 && Number (userAge) <= 150):
+                          if (Number (userAge) > 50 && Number (userAge) <= 150){
+                            alert( " WOW !! " + userName + " , " + sage1 + " , " + " à " + Number (userAge) + " ans !" + " n'est ce pas une sagesse inestimable, alors que les jours passent !!! ");
+                          }
+                        break;
+        
+                        default:
+                          alert( " Uuuups!!!   Nous ne prenons que les valeurs entrées qu'en nombres Entiers ! rechargez la page, Réesayez avec votre age Véritable  en Nombre Entiers, Merci de nous faire confiance ! ");
+        }
+    }
+    
