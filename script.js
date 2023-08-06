4e266ed9c92b7883e22295a2a4cad2da0d8ed77a
Thierry Murhondezi Bisimwa <thierrymurhondezi@gmail.com>
2023-08-06 05:02:02 +0200

    feat: 12 thist  switch case condtition

--- script.js
+++ script.js
@@ -103,3 +103,210 @@ alert( "Bien venu dans notre programme Qui Booste votre Motivatoin journaliere a
         break;
       };
     break
+
+
+    case 3:
+        let sage4= (" « Montrez-vous, montrez-vous, montrez-vous, et au bout d'un moment, la muse se montrera également. » — Une citation motivante d'Isabel Allende ")
+                                
+        switch (true) {
+          case Number(userAge) > 0 && Number(userAge) <= 25:
+            alert( " ' " + userName + "!! ' " + ", " + sage4 + ", n'est-ce pas une très belle citation à connaître à " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 25 && Number(userAge) <= 50:
+            alert( " ' " + userName + "!! ' " + ", " + sage4 + ", Géniale citation à connaître maintenant que tu as " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 50 && Number(userAge) <= 150:
+            alert("WOW !! " + " ' " + userName + "!! ' "  + ", " + sage4 + " à " + Number(userAge) + " ans! N'est-ce pas une sagesse inestimable, alors que les jours passent !!!");
+            break;
+    
+          default:
+          alert("Uuuups!!! Nous ne prenons que les valeurs entrées qu'en nombres entiers! Rechargez la page, réessayez avec votre âge véritable en nombres entiers. Merci de nous faire confiance !");
+          break;
+        };
+    
+        break
+      
+        case 4: 
+          let sage5=" « Celui qui se perd dans sa passion a moins perdu que celui qui perd sa passion. » — Une citation de sagesse d'Alexandre Jardin"
+          
+          switch (true) {
+          case Number(userAge) > 0 && Number(userAge) <= 25:
+            alert( " ' " + userName + "!! ' " + ", " + sage5 + ", n'est-ce pas une très belle citation à connaître à " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 25 && Number(userAge) <= 50:
+            alert( " ' " + userName + "!! ' " + ", " + sage5 + ", Géniale citation à connaître maintenant que tu as " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 50 && Number(userAge) <= 150:
+            alert("WOW !! " + " ' " + userName + "!! ' "  + ", " + sage5 + " à " + Number(userAge) + " ans! N'est-ce pas une sagesse inestimable, alors que les jours passent !!!");
+            break;
+    
+          default:
+          alert("Uuuups!!! Nous ne prenons que les valeurs entrées qu'en nombres entiers! Rechargez la page, réessayez avec votre âge véritable en nombres entiers. Merci de nous faire confiance !");
+          break;
+        };
+          
+          break
+    
+        case 5:
+          let sage6=" « Si vous voulez que la vie vous sourie, apportez-lui d'abord votre bonne humeur. » — Une belle citation de Baruch Spinoza   "
+            switch (true) {
+          case Number(userAge) > 0 && Number(userAge) <= 25:
+            alert( " ' " + userName + "!! ' " + ", " + sage6 + ", n'est-ce pas une très belle citation à connaître à " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 25 && Number(userAge) <= 50:
+            alert( " ' " + userName + "!! ' " + ", " + sage6 + ", Géniale citation à connaître maintenant que tu as " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 50 && Number(userAge) <= 150:
+            alert("WOW !! " + " ' " + userName + "!! ' "  + ", " + sage6 + " à " + Number(userAge) + " ans! N'est-ce pas une sagesse inestimable, alors que les jours passent !!!");
+            break;
+    
+          default:
+          alert("Uuuups!!! Nous ne prenons que les valeurs entrées qu'en nombres entiers! Rechargez la page, réessayez avec votre âge véritable en nombres entiers. Merci de nous faire confiance !");
+          break;
+        };                          
+          
+          break
+    
+          case 6:
+            let sage7=" « Si vous pensez que quelque chose doit exister, si c'est quelque chose que vous allez utiliser vous-même, ne laissez personne vous dissuader de tenter le coup. » — Une citation encourageante de Tobias Lutke "
+              
+            switch (true) {
+          case Number(userAge) > 0 && Number(userAge) <= 25:
+            alert( " ' " + userName + "!! ' " + ", " + sage7 + ", n'est-ce pas une très belle citation à connaître à " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 25 && Number(userAge) <= 50:
+            alert( " ' " + userName + "!! ' " + ", " + sage7 + ", Géniale citation à connaître maintenant que tu as " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 50 && Number(userAge) <= 150:
+            alert("WOW !! " + " ' " + userName + "!! ' "  + ", " + sage7 + " à " + Number(userAge) + " ans! N'est-ce pas une sagesse inestimable, alors que les jours passent !!!");
+            break;
+    
+          default:
+          alert("Uuuups!!! Nous ne prenons que les valeurs entrées qu'en nombres entiers! Rechargez la page, réessayez avec votre âge véritable en nombres entiers. Merci de nous faire confiance !");
+          break;
+        };
+            
+            break
+    
+        case 7:
+          let sage8= " « Il n'y a qu'une chose qui puisse rendre un rêve impossible, c'est la peur d'échouer. » ― une citation motivante de Paulo Coelho, écrivain brésilien "
+            
+          switch (true) {
+          case Number(userAge) > 0 && Number(userAge) <= 25:
+            alert( " ' " + userName + "!! ' " + ", " + sage7 + ", n'est-ce pas une très belle citation à connaître à " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 25 && Number(userAge) <= 50:
+            alert( " ' " + userName + "!! ' " + ", " + sage7 + ", Géniale citation à connaître maintenant que tu as " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 50 && Number(userAge) <= 150:
+            alert("WOW !! " + " ' " + userName + "!! ' "  + ", " + sage7 + " à " + Number(userAge) + " ans! N'est-ce pas une sagesse inestimable, alors que les jours passent !!!");
+            break;
+    
+          default:
+          alert("Uuuups!!! Nous ne prenons que les valeurs entrées qu'en nombres entiers! Rechargez la page, réessayez avec votre âge véritable en nombres entiers. Merci de nous faire confiance !");
+          break;
+        };
+    
+          break
+    
+        case 8:
+          let sage9=" « Le meilleur chemin est toujours celui qui est le plus direct. » — Une citation de motivation de Robert Frost "
+              
+            switch (true) {
+          case Number(userAge) > 0 && Number(userAge) <= 25:
+            alert( " ' " + userName + "!! ' " + ", " + sage9 + ", n'est-ce pas une très belle citation à connaître à " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 25 && Number(userAge) <= 50:
+            alert( " ' " + userName + "!! ' " + ", " + sage9 + ", Géniale citation à connaître maintenant que tu as " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 50 && Number(userAge) <= 150:
+            alert("WOW !! " + " ' " + userName + "!! ' "  + ", " + sage9 + " à " + Number(userAge) + " ans! N'est-ce pas une sagesse inestimable, alors que les jours passent !!!");
+            break;
+    
+          default:
+          alert("Uuuups!!! Nous ne prenons que les valeurs entrées qu'en nombres entiers! Rechargez la page, réessayez avec votre âge véritable en nombres entiers. Merci de nous faire confiance !");
+          break;
+        };
+          
+          break
+    
+        case 9:
+          let sage10=console.log("« N'imitez rien ni personne. Un lion qui copie un lion devient un singe. »  citation audacieuse de Victor Hugo, écrivain français ")
+            
+          switch (true) {
+          case Number(userAge) > 0 && Number(userAge) <= 25:
+            alert( " ' " + userName + "!! ' " + ", " + sage10 + ", n'est-ce pas une très belle citation à connaître à " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 25 && Number(userAge) <= 50:
+            alert( " ' " + userName + "!! ' " + ", " + sage10 + ", Géniale citation à connaître maintenant que tu as " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 50 && Number(userAge) <= 150:
+            alert("WOW !! " + " ' " + userName + "!! ' "  + ", " + sage10 + " à " + Number(userAge) + " ans! N'est-ce pas une sagesse inestimable, alors que les jours passent !!!");
+            break;
+    
+          default:
+          alert("Uuuups!!! Nous ne prenons que les valeurs entrées qu'en nombres entiers! Rechargez la page, réessayez avec votre âge véritable en nombres entiers. Merci de nous faire confiance !");
+          break;
+        };
+          
+          break
+    
+        case 10:
+          let sage11=" « L'art commence avec la difficulté. »— une citation sur la ténacité de André Gide, écrivain français "
+           
+          switch (true) {
+          case Number(userAge) > 0 && Number(userAge) <= 25:
+            alert( " ' " + userName + "!! ' " + ", " + sage11 + ", n'est-ce pas une très belle citation à connaître à " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 25 && Number(userAge) <= 50:
+            alert( " ' " + userName + "!! ' " + ", " + sage11 + ", Géniale citation à connaître maintenant que tu as " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 50 && Number(userAge) <= 150:
+            alert("WOW !! " + " ' " + userName + "!! ' "  + ", " + sage11 + " à " + Number(userAge) + " ans! N'est-ce pas une sagesse inestimable, alors que les jours passent !!!");
+            break;
+    
+          default:
+          alert("Uuuups!!! Nous ne prenons que les valeurs entrées qu'en nombres entiers! Rechargez la page, réessayez avec votre âge véritable en nombres entiers. Merci de nous faire confiance !");
+          break;
+        };
+          
+          break
+    
+        case 11:
+          let sage12=" « Quelqu'un va déclarer : “Je suis le leader !” et s'attendre à ce que tout le monde le suive aveuglément. Selon mon expérience, ce n'est pas ainsi que ça se passe. Les autres vous suivront en fonction de la qualité de vos actions plutôt que la magnitude de vos déclarations. » — Une citation de bon sens de Bill Walsh  "
+              
+            switch (true) {
+          case Number(userAge) > 0 && Number(userAge) <= 25:
+            alert( " ' " + userName + "!! ' " + ", " + sage12 + ", n'est-ce pas une très belle citation à connaître à " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 25 && Number(userAge) <= 50:
+            alert( " ' " + userName + "!! ' " + ", " + sage12 + ", Géniale citation à connaître maintenant que tu as " + Number(userAge) + " ans !?");
+            break;
+    
+          case Number(userAge) > 50 && Number(userAge) <= 150:
+            alert("WOW !! " + " ' " + userName + "!! ' "  + ", " + sage12 + " à " + Number(userAge) + " ans! N'est-ce pas une sagesse inestimable, alors que les jours passent !!!");
+            break;
+    
+          default:
+          alert("Uuuups!!! Nous ne prenons que les valeurs entrées qu'en nombres entiers! Rechargez la page, réessayez avec votre âge véritable en nombres entiers. Merci de nous faire confiance !");
+          break;
+        };
+    
+          break
